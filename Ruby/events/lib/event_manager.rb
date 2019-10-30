bbdabf6058f0835a95ea299d46fca329604a994a
ldust667 <ldust667@gmail.com>
2019-10-30 13:03:28 -0400

    Logic change for first Line

--- Ruby/events/lib/event_manager.rb
+++ Ruby/events/lib/event_manager.rb
@@ -2,7 +2,14 @@ puts "Events Manager Intialized!"
 
 content= File.readlines "event_attendees.csv"
 
-content.each do |record|
+#index = 0
+
+content.each_with_index do |record,index|
+#omitting the first line headers in csv
+#next if record == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
+#index = index+1
+#next if index == 1	
+	next if index == 0
 	columns = record.split(",")
 	first_name= columns[2]
 #delimiting csv file to attain first name of each record
