289cae021e30c09cd3611f37729aa3cff457f182
ldust667 <ldust667@gmail.com>
2019-10-30 13:22:33 -0400

    Adding header converters and variables refrences in quotes

--- Ruby/events/lib/event_manager.rb
+++ Ruby/events/lib/event_manager.rb
@@ -5,9 +5,11 @@ content= File.readlines "attendees.csv"
 
 
 
-content= CSV.open "attendees.csv", headers: true
+content= CSV.open "attendees.csv", headers: true, header_converters: :symbol
 
 content.each do |row|
-	name = row[2]
-	puts name
+	
+	name = row[:first_name]
+	zipcode = row[:zipcode]
+	puts "#{name} #{zipcode}"
 end
