1daaca32df4c9fc8fce96c0055f36990d7bb0ac4
ldust667 <ldust667@gmail.com>
2019-10-30 13:37:37 -0400

    Moving Logic to Method

--- Ruby/events/lib/event_manager.rb
+++ Ruby/events/lib/event_manager.rb
@@ -1,4 +1,21 @@
 require "csv"
+
+
+def clean_zip(zipcode)
+	if zipcode.nil?
+		zipcode= "00000"
+        elsif zipcode.length < 5
+                zipcode = zipcode.rjust 5, "0"
+        elsif zipcode.length > 5
+                zipcode = zipcode[0..4]
+	else
+		zipcode
+        end
+end
+
+
+
+
 puts "Events Manager Intialized!"
 
 content= File.readlines "attendees.csv"
@@ -10,15 +27,7 @@ content= CSV.open "attendees.csv", headers: true, header_converters: :symbol
 content.each do |row|
 	
 	name = row[:first_name]
-	zipcode = row[:zipcode]
-
-	if zipcode.nil?
-		zipcode= "00000"
-	elsif zipcode.length < 5
-		zipcode = zipcode.rjust 5, "0"
-	elsif zipcode.length > 5
-		zipcode = zipcode[0..4]
-	end
+	zipcode = clean_zip(row[:zipcode])
 
 
 	puts "#{name} #{zipcode}"
