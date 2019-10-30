be3de798ba3ff075f39f4559ac2ac9b5ff60db1d
ldust667 <ldust667@gmail.com>
2019-10-30 11:46:28 -0400

    split p

--- Ruby/events/lib/event_manager.rb
+++ Ruby/events/lib/event_manager.rb
@@ -3,5 +3,7 @@ puts "Events Manager Intialized!"
 content= File.readlines "event_attendees.csv"
 
 content.each do |record|
-	puts record
+	columns = record.split(",")
+#using P instead of puts to show the values as the arrays they actually are
+	p columns
 end
