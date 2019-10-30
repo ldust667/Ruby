c310a4137c8ba60d0dbd66050b4d65fd9aeedc62
ldust667 <ldust667@gmail.com>
2019-10-30 13:04:47 -0400

    Cleaning up comments

--- Ruby/events/lib/event_manager.rb
+++ Ruby/events/lib/event_manager.rb
@@ -2,13 +2,10 @@ puts "Events Manager Intialized!"
 
 content= File.readlines "event_attendees.csv"
 
-#index = 0
+
 
 content.each_with_index do |record,index|
 #omitting the first line headers in csv
-#next if record == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
-#index = index+1
-#next if index == 1	
 	next if index == 0
 	columns = record.split(",")
 	first_name= columns[2]
