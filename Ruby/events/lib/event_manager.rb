4556752e1461c146896a11abc0c1130b071508c5
ldust667 <ldust667@gmail.com>
2019-11-05 09:50:12 -0500

    Method transition with verbose error checking

--- Ruby/events/lib/event_manager.rb
+++ Ruby/events/lib/event_manager.rb
@@ -1,15 +1,45 @@
 require "csv"
 require 'google/apis/civicinfo_v2'
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-#setting API Key
-civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
-
 
 
 def clean_zip(zipcode)
 	#combined logic to convert value to string add zeros to the front up to the length of 5 and cut off vallues past 5 characters
 	zipcode.to_s.rjust(5,"0")[0..4]	
+
+end
+
+
+
+def legislator_zip_prnt(name,zip)
+	civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+	#setting API Key
+	civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+	begin
+		legislators = civic_info.representative_info_by_address(
+			address: zip,
+                	levels: 'country',
+                	roles: ['legislatorUpperBody', 'legislatorLowerBody' ])
+                
+		#calling method to access list
+                legislators = legislators.officials
+
+                #store legislature names from list
+                legislator_name= legislators.map(&:name)
+	#	puts legislator_name
+
+                #converting to string and comma seperating
+                legislator_name.join(", ")
+
+                #print "#{name} #{zipcode} Your elected Officials: "
+                #puts "#{legislators_str}"
+
+
+        rescue
+                #if the above fails(most likely due to a zipcode error print an error message and direct the client to a web page | Data should be handled with records beforehand but for the time being it is handled this way
+                puts "#{name}, you can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials. There appears to be an issue with the zipcode provided: #{zip}"
+        end
 
 end
 
@@ -25,29 +55,10 @@ content.each do |row|
 	
 	name = row[:first_name]
 	zipcode = clean_zip(row[:zipcode])
-
-	
-	begin
-		legislators = civic_info.representative_info_by_address(
-				address: zipcode,
-				levels: 'country',
-				roles: ['legislatorUpperBody', 'legislatorLowerBody' ])
-		#calling method to access list
-		legislators = legislators.officials 
-
-		#store legislature names from list 
-		legislator_name= legislators.map(&:name)
-		
-		#converting to string and comma seperating
-		legislators_str = legislator_name.join(", ")
- 
-		print "#{name} #{zipcode} Your elected Officials: "
-		puts "#{legislators_str}"
-
-
-	rescue
-		puts "#{name}, you can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials. There appears to be an issue with the zipcode provided: #{zipcode}"
+	if legislators = legislator_zip_prnt(name,zipcode)
+		puts "#{name} #{zipcode} #{legislators} "
 	end
+        #puts "#{legislators}"
 
-
+		
 end
